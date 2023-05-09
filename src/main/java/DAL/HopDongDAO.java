4604d239f85b06cfce1052362324f86e21137a72
Poem179 <128458334+Poem179@users.noreply.github.com>
2023-05-09 23:28:59 +0700

    Update HopDongDAO.java

--- src/main/java/DAL/HopDongDAO.java
+++ src/main/java/DAL/HopDongDAO.java
@@ -15,19 +15,20 @@ public class HopDongDAO extends AbstractDAO<HopDong, Integer>{
     @Override
 
     public boolean insert(HopDong hd) {
-        String query = "INSERT INTO Hop_Dong(MaHD, MaLoaiHD, MaNV, TenHD, NgayKy, HanHD, HocVi, Hang, Bac, MaSoThue, CucThue, STK, NganHang, SoTietGiangDay) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        String query = "INSERT INTO Hop_Dong(MaHD, MaNV, TenHD, NgayKy, HanHD,HeSoLuong, HeSoPhuCap) VALUES (?,?,?,?,?,?,?)";
         mySQLConnection.openConnection();
-        int rowInserted = mySQLConnection.executeUpdate(query, hd.getMaHD(), hd.getMaLoaiHD(), hd.getMaNV(), hd.getTenHD(), hd.getNgayKy(), hd.getHanHD(), hd.getHocVi(), hd.getHang(), hd.getBac(), hd.getMaSoThue()
-            , hd.getCucThue(), hd.getSTK(), hd.getNganHang(), hd.getSoTietGiangDay());
+        int rowInserted = mySQLConnection.executeUpdate(query, hd.getMaHD(), hd.getMaNV(), hd.getTenHD(), hd.getNgayKy(), hd.getHanHD()
+            ,hd.getHeSoLuong(),hd.getHeSoPhuCap());
         mySQLConnection.closeConnection();
         return rowInserted > 0;
     }
 
     @Override
     public boolean update(HopDong hd) {
-        String query = "UPDATE Hop_Dong SET MaHD = ?, MaLoaiHD = ?, MaNV = ?, TenHD = ?, NgayKy = ?, HanHD = ?, HocVi = ?, Hang = ?, Bac = ?, MaSoThue = ?, CucThue = ?, STK = ?, NganHang = ?, SoTietGiangDay = ? WHERE MaHD = ?";
+        String query = "UPDATE Hop_Dong SET MaNV, TenHD, NgayKy, HanHD,HeSoLuong, HeSoPhuCap WHERE MaHD = ?";
         mySQLConnection.openConnection();
-        int rowUpdated = mySQLConnection.executeUpdate(query, hd.getMaHD(), hd.getMaLoaiHD(),hd.getMaNV(),hd.getTenHD(),hd.getNgayKy(),hd.getHanHD(),hd.getHocVi(),hd.getHang(),hd.getBac(),hd.getMaSoThue(),hd.getCucThue(),hd.getSTK(),hd.getNganHang(),hd.getSoTietGiangDay());
+        int rowUpdated = mySQLConnection.executeUpdate(query,  hd.getMaNV(), hd.getTenHD(), hd.getNgayKy(), hd.getHanHD()
+            ,hd.getHeSoLuong(),hd.getHeSoPhuCap());
         mySQLConnection.closeConnection();
         return rowUpdated > 0;
     }
@@ -49,28 +50,30 @@ public class HopDongDAO extends AbstractDAO<HopDong, Integer>{
     @Override
     public List<HopDong> selectAll() {
         List<HopDong> list = new ArrayList<HopDong>();
-        String query = "INSERT INTO Hop_Dong(MaHD, MaLoaiHD, MaNV, TenHD, NgayKy, HanHD, HocVi, Hang, Bac, MaSoThue, CucThue, STK, NganHang, SoTietGiangDay) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        String query = "SELECT * FROM Hop_Dong";
         mySQLConnection.openConnection();
-        ResultSet rs = mySQLConnection.executeQuery(query, LocalDate.now().getMonthValue(), LocalDate.now().getYear());
+        ResultSet rs = mySQLConnection.executeQuery(query);
         try {
             while (rs.next()){
                 var MaHD = rs.getString("MaHD");
-                var MaLoaiHD = rs.getString("MaLoaiHD");
+//                var MaLoaiHD = rs.getString("MaLoaiHD");
                 var MaNV = rs.getString("MaNV");
                 var TenHD = rs.getString("TenHD");
                 var NgayKy = rs.getString("NgayKy");
                 var HanHD = rs.getString("HanHD");
-                var HocVi = rs.getString("HocVi");
-                var Hang = rs.getString("Hang");
-                var Bac = rs.getString("Bac");
-                var MaSoThue = rs.getString("MaSoThue");
-                var CucThue = rs.getString("CucThue");
-                var STK = rs.getString("STK");
-                var NganHang = rs.getString("NganHang");
-                var SoTietGiangDay = rs.getString("SoTietGiangDay");
+//                var HocVi = rs.getString("HocVi");
+//                var Hang = rs.getString("Hang");
+//                var Bac = rs.getString("Bac");
+//                var MaSoThue = rs.getString("MaSoThue");
+//                var CucThue = rs.getString("CucThue");
+//                var STK = rs.getString("STK");
+//                var NganHang = rs.getString("NganHang");
+//                var CongViecDuocGiao =rs.getString("CongViecDuocGiao");
+                var HeSoLuong =rs.getString("HeSoLuong");
+                var HeSoPhuCap =rs.getString("HeSoPhuCap");
              
-                HopDong nv = new HopDong(MaHD, MaLoaiHD, MaNV, TenHD, NgayKy, HanHD, HocVi, Hang, Bac, MaSoThue
-            , CucThue, STK, NganHang, SoTietGiangDay);
+                HopDong nv;
+                nv = new HopDong(MaHD, MaNV, TenHD, NgayKy, HanHD, HeSoLuong, HeSoPhuCap);
                         list.add(nv);
             }
         } catch (SQLException e) {
