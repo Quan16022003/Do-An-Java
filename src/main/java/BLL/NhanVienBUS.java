f7247d1905c5408463ad310ba50099fde36f2d5e
Poem179 <128458334+Poem179@users.noreply.github.com>
2023-05-09 23:26:56 +0700

    Update NhanVienBUS.java

--- src/main/java/BLL/NhanVienBUS.java
+++ src/main/java/BLL/NhanVienBUS.java
@@ -4,7 +4,9 @@
  */
 package BLL;
 
-import DTO.NhanVien;
+import DAL.HopDongDAO;
+import DTO.HopDong;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
@@ -12,29 +14,87 @@ import javax.swing.JOptionPane;
  *
  * @author apple
  */
-public class NhanVienBUS extends javax.swing.JDialog{
-    public boolean check(NhanVien nv){
-        
+public class HopDongBUS extends javax.swing.JDialog{
+    private List<HopDong> HopDongs;
+    private HopDongDAO Dao;
+    public boolean check(HopDong nv){
+        Dao = new HopDongDAO();
+        System.out.println("Checking...");
+        HopDongs = Dao.selectAll();
+        for (HopDong key : HopDongs)
+        if (key.getMaHD().equals(nv.getMaHD()))
+            {
+                   JOptionPane.showMessageDialog(rootPane, "Mã hợp đồng này đang được sử dụng "); return false;
+            } 
+        if (Pattern.matches("[a-z0-9]*",nv.getMaNV())==false)
+            {
+                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : mã nhân viên "); return false;
+            }
+        if (Pattern.matches("[a-z0-9]*",nv.getMaHD())==false)
+            {
+                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : mã hợp đồng"); return false;
+            }
+//        if (Pattern.matches("\\d{12}",nv.getSTK())==false)
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : số tài khoản"); return false;
+//            }
+//        if (Pattern.matches("[a-z0-9\\s]*",nv.getHocVi())==false)
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : học vị"); return false;
+//            }
+//        if (Pattern.matches("[a-z0-9]*",nv.getMaSoThue())==false)
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : mã số thuế"); return false;
+//            }
+//        if (Pattern.matches("[a-z0-9\\s]*",nv.getNganHang())==false)     
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : ngân hàng"); return false;
+//            }
+        if (Pattern.matches("[a-z0-9\\s]*",nv.getTenHD())==false)
+            {
+                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : tên hợp đồng"); return false;
+            }
+//        if (Pattern.matches("[a-z0-9\\s]*",nv.getCucThue())==false)
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : cục thuế"); return false;
+//            }
+        if (Pattern.matches("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$",nv.getNgayKy())==false)
+            {
+                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : ngày ký"); return false;
+            }
+        if (Pattern.matches("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$",nv.getHanHD())==false)
+            {
+                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : hạn dùng"); return false;
+            }
+//        if (Pattern.matches("[a-z0-9\\s]*",nv.getBac())==false)
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : bậc"); return false;
+//            }
+//        if (Pattern.matches("[+-]?([0-9]*[.])?[0-9]+",nv.getHeSoLuong())==false)
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : hệ số lương"); return false;
+//            }
+//        if (Pattern.matches("[+-]?([0-9]*[.])?[0-9]+",nv.getHeSoPhuCap())==false)
+//            {
+//                   JOptionPane.showMessageDialog(rootPane, "Bạn nhập không hợp lệ : hệ số phụ cấp"); return false;
+//            }    
         
         if (              
-                  nv.getMaNV().isEmpty()
-                |nv.getMaDV().isEmpty()
-                |nv.getMaLoaiNV().isEmpty()
-                |nv.getMaChucVu().isEmpty()
-                |nv.getTen().isEmpty()
-                |nv.getGioiTinh().isEmpty()
-                |nv.getNgSinh().isEmpty()
-                |nv.getSDT().isEmpty()
-                |nv.getSoNha().isEmpty()
-                |nv.getDuong().isEmpty()
-                |nv.getPhuong_Xa().isEmpty()
-                |nv.getQuan_Huyen().isEmpty()
-                |nv.getTP_Tinh().isEmpty()
-                |nv.getCCCD().isEmpty()
-                |nv.getNgCap().isEmpty()
-                |nv.getNoiCap().isEmpty()
-                |nv.getNgVaoLam().isEmpty()
-                |nv.getImage().isEmpty()
+                  nv.getMaHD().isEmpty()
+//                |nv.getMaLoaiHD().isEmpty()
+                |nv.getMaNV().isEmpty()
+                |nv.getNgayKy().isEmpty()
+                |nv.getHanHD().isEmpty()
+//                |nv.getHocVi().isEmpty()
+//                |nv.getHang().isEmpty()
+//                |nv.getBac().isEmpty()
+//                |nv.getMaSoThue().isEmpty()
+//                |nv.getCucThue().isEmpty()
+//                |nv.getSTK().isEmpty()
+//                |nv.getNganHang().isEmpty()
+                |nv.getTenHD().isEmpty()
+//                |nv.getHeSoLuong().isEmpty()
+//                |nv.getHeSoPhuCap().isEmpty()
               )
                return false;
            return true;
